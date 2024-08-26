969faaf669039f586a947a09417baab83ba7ce08
Hassan Mohamed <hmohammed.c@tetco.sa>
2024-08-26 07:32:06 +0300

    Add Get Product By Id Feature

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Catalog.API.Products.GetProductById;
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+public record GetProductByIdResult(Product Product);
+
+internal sealed class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
+
+        var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+        return product is { } ? new GetProductByIdResult(product) : throw new ProductNotFoundException();
+    }
+}
