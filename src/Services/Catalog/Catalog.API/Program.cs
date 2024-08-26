0c05f2d8e2d8d67abadac1923fd035c15b41f91d
Hassan Mohamed <hmohammed.c@tetco.sa>
2024-08-26 17:40:41 +0300

    Global Handling Exceptions use app.ExceptionHandler

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMediatR(config =>
@@ -16,6 +19,31 @@ builder.Services.AddMarten(opts =>
 }).UseLightweightSessions();
 
 var app = builder.Build();
+
 app.MapCarter();
+
+app.UseExceptionHandler(config =>
+{
+    config.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if (exception is null) return;
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = exception.Message,
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = exception.StackTrace
+        };
+
+        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+        logger.LogError(exception, exception.Message);
+
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/json";
+
+        await context.Response.WriteAsJsonAsync(problemDetails);
+    });
+});
 
 await app.RunAsync();
