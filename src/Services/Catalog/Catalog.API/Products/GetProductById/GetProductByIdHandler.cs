6a61aaacfb51d7b4672d32c0b4c410be0112ae52
Hassan Mohamed <hmohammed.c@tetco.sa>
2024-08-27 07:30:19 +0300

    Add Logging Behavior as Cross Cutting and remove all injected logger to command handlers

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -2,12 +2,10 @@
 public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
 public record GetProductByIdResult(Product Product);
 
-internal sealed class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+internal sealed class GetProductByIdQueryHandler(IDocumentSession session) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
 {
     public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
     {
-        logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
-
         var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
         return product is { } ? new GetProductByIdResult(product) : throw new ProductNotFoundException(query.Id);
     }
