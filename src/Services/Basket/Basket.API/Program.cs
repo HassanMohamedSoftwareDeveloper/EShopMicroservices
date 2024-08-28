fb2a3a185728cdc096428137cb537a082be203d7
Hassan Mohamed <hmohammed.c@tetco.sa>
2024-08-28 14:49:03 +0300

    Add Basket Use Cases
    - Get Basket
    - Store Basket
    - Delete Basket

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,4 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
 var app = builder.Build();
+
+app.MapCarter();
 
 await app.RunAsync();
