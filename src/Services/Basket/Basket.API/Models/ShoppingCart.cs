85f2d1e5e2a67de3a00c6581adb49d9b982eea1a
Hassan Mohamed <hmohammed.c@tetco.sa>
2024-08-28 10:24:37 +0300

    Add Domain Models

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,25 @@
+﻿namespace Basket.API.Models;
+
+public class ShoppingCart
+{
+    #region PROPS :
+
+    public string UserName { get; set; } = default!;
+    public List<ShoppingCartItem> Items { get; set; } = [];
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+
+    #endregion PROPS :
+
+    #region CTORS :
+
+    public ShoppingCart(string userName)
+    {
+        this.UserName = userName;
+    }
+
+    public ShoppingCart()
+    {
+    }
+
+    #endregion CTORS :
+}
