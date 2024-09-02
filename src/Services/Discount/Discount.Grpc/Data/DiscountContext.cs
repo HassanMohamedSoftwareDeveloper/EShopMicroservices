01b556855feecfc6ef2d4b607461d9f83f54bdaa
Hassan Mohamed <hmohammed.c@tetco.sa>
2024-09-02 22:09:51 +0300

    Add context and seed data

--- src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -7,4 +7,16 @@ public class DiscountContext(DbContextOptions<DiscountContext> options) : DbCont
     public DbSet<Coupon> Coupons { get; set; }
 
     #endregion PROPS :
+
+    #region Methods :
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Coupon>().HasData(
+            new Coupon { Id = 1, ProductName = "IPhone X", Description = "IPhone Discount", Amount = 150 },
+            new Coupon { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount", Amount = 100 }
+            );
+    }
+
+    #endregion Methods :
 }
--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -6,8 +6,8 @@ builder.Services.AddDbContext<DiscountContext>(opts => opts.UseSqlite(builder.Co
 
 var app = builder.Build();
 
+await app.UseMigrations();
 app.MapGrpcService<DiscountService>();
-
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 await app.RunAsync();
