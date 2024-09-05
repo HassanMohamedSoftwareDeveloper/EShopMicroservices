fea09155cba33d8494877538dbbe3b6d65d94dbb
Hassan Mohamed <hmohammed.c@tetco.sa>
2024-09-05 18:02:16 +0300

    Add Domain models,enums and values objects

---
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -0,0 +1,28 @@
+﻿namespace Ordering.Domain.Models;
+
+public class Order : Aggregate<Guid>
+{
+    #region Fields :
+
+    private readonly List<OrderItem> _orderItems = [];
+
+    #endregion Fields :
+
+    #region PROPS :
+
+    public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
+    public Guid CustomerId { get; private set; } = default!;
+    public string OrderName { get; private set; } = default!;
+    public Address ShippingAddress { get; private set; } = default!;
+    public Address BillingAddress { get; private set; } = default!;
+    public Payment Payment { get; private set; } = default!;
+    public OrderStatus Status { get; private set; } = OrderStatus.Pending;
+
+    public decimal TotalPrice
+    {
+        get => OrderItems.Sum(x => x.TotalPrice);
+        private set { }
+    }
+
+    #endregion PROPS :
+}
