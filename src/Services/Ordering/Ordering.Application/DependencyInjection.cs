5e05ce78cf0c911f16d6f203f75960ea2ee05f6d
Hassan Mohamed <hmohammed.c@tetco.sa>
2024-09-07 01:47:41 +0300

    add DispatchDomainEventsInterceptor

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,9 +1,15 @@
-﻿namespace Ordering.Application;
+﻿using System.Reflection;
+
+namespace Ordering.Application;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
         return services;
     }
 }
